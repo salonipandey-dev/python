d48615c5daade96398039098faaf9cd0e87cb3ad
Saloni paney-ux <workspaceworkout0716@gmail.com>
2026-03-06 21:39:54 +0530

    updates

---
+++ Variablesanddatatypes/datatypes.py
@@ -0,0 +1,15 @@
+a=1 #integer
+b=2.3 #float
+c="Hello" #string
+d=True #boolean
+e=None #NoneType
+print(a)
+print(b)
+print(c)
+print(d)
+print(e)
+print(type(a))
+print(type(b))
+print(type(c)) 
+print(type(d))  
+print(type(e))
