51252ea068a6e1b8339de6b91a1b008c17791f96
Saloni paney-ux <workspaceworkout0716@gmail.com>
2026-03-06 22:00:51 +0530

    typecastingdone

---
+++ typefunctionsansandtypecasting/typecasting.py
@@ -0,0 +1,13 @@
+a="6784833"
+print(a)
+print(type(a))
+b=int(a)
+print(b)
+#here we used int() function to convert the string variable a into an integer variable b.
+c=float(a)
+print(c)
+#here we used float() function to convert the string variable a into a float variable c.
+#typecasting is the process of converting one data type into another data type. In this code we used int() and float() functions to convert string variable a into integer and float variables respectively.    
+d=str(c)
+print(d)
+print(type(d))
---
+++ typefunctionsansandtypecasting/typefunctions.py
@@ -0,0 +1,8 @@
+a="this is a string."
+c=type(a)
+print(c)
+#here we used type() function to find the datatype of variable a which is string. 
+# and c are variables and type() is a function which is used to find the datatype of variable a.      
+b=12
+print(type(b))
+#int type()
