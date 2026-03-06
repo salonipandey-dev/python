f94f17699edf0ce63d83030b84c349853960683d
Saloni paney-ux <workspaceworkout0716@gmail.com>
2026-03-06 22:39:10 +0530

    kk

---
+++ Strings/skipvalue.py
@@ -0,0 +1,5 @@
+#there is concept of skip value in string which is used to skip the value of string and print the rest of the string. It is denoted by : and the value which we want to skip is written before : and the rest of the string is printed after :
+str="saloni"    
+print(str[::2])  #it will skip the value of string and print the rest of the string
+a="hithereitsnotawaytowriteastring"
+print(a[1::3])  #it will skip the value of string and print the rest of the string"
