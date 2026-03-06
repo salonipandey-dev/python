7cd9359e98674242d35d3ec0a5e12858f59d456e
Saloni paney-ux <workspaceworkout0716@gmail.com>
2026-03-06 22:14:01 +0530

    updates

---
+++ Variablesanddatatypes/practiceset/checktype.py
@@ -0,0 +1,5 @@
+# Check the type of variable assigned using input () function.
+a=input("enter the value of a:")
+print(a)    
+print(type(a))
+#here we used input() function to take input from user and type() function to find the
---
+++ Variablesanddatatypes/practiceset/remainder.py
@@ -0,0 +1,5 @@
+#Write a python program to find remainder when a number is divided by z.
+a=int(input("enter the number:"))
+z=int(input("enter the divisor:"))  
+remainder=a%z
+print(remainder)
