bd97d50e060d8dc229580ca819d39a99b3d32bf6
Saloni paney-ux <workspaceworkout0716@gmail.com>
2026-03-06 21:44:39 +0530

    update

---
+++ Variablesanddatatypes/operators/arithmetic.py
@@ -0,0 +1,10 @@
+a=10
+b=5
+print("addition:",a+b)
+print("subtraction:",a-b)
+print("multiplication:",a*b)
+print("division:",a/b)
+print("modulus:",a%b)
+print("floor division:",a//b)
+print("exponentiation:",a**b)
+#here we used all kind of arithmetic operators in python.
