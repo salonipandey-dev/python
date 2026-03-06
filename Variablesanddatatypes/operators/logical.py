00844f675e376df393012caeddb81ebe9581bf57
Saloni paney-ux <workspaceworkout0716@gmail.com>
2026-03-06 21:53:55 +0530

    true or false

---
+++ Variablesanddatatypes/operators/logical.py
@@ -0,0 +1,6 @@
+a=True
+b=False
+print("Logical AND:",a and b)
+print("Logical OR:",a or b)
+print("Logical NOT:",not a)
+#here we used all kind of logical operators in python while comparing two variables a and b.
