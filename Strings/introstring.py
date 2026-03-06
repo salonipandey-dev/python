f99939abd20322ce8bdb3c3a9d051853f747e093
Saloni paney-ux <workspaceworkout0716@gmail.com>
2026-03-06 22:31:09 +0530

    dnenen

---
+++ Strings/introstring.py
@@ -0,0 +1,19 @@
+str="saloni"
+print(str)
+#print string
+print(type(str))
+#type of variable
+#indexing
+print(str[0])
+print(str[1])
+print(str[2])
+print(str[3])
+#string slicing
+print(str[0:3])
+print(str[1:5]) 
+print(str[0:])
+print(str[:5])
+print(str[:])   
+#negative slicing
+print(str[-5:-2])
+print(str[-2]) 
