12ad247fec982d988ece4ce97def051bcba04aa1
Saloni paney-ux <workspaceworkout0716@gmail.com>
2026-03-06 21:50:30 +0530

    done

---
+++ Variablesanddatatypes/operators/comparsion.py
@@ -0,0 +1,9 @@
+a=10
+b=3
+print("Equal to:",a==b)
+print("Not equal to:",a!=b) 
+print("Greater than:",a>b)
+print("Less than:",a<b)
+print("Greater than or equal to:",a>=b)
+print("Less than or equal to:",a<=b)
+#here we used all kind of comparison operators in python while comparing two variables a and b.
