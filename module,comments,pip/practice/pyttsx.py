3202876a8d0fa240ae8aa0f54a1b22d56cb2cc52
Saloni paney-ux <workspaceworkout0716@gmail.com>
2026-03-06 21:31:00 +0530

    updates

--- module,comments,pip/practice/pyttsx.py
+++ module,comments,pip/practice/pyttsx.py
@@ -2,5 +2,5 @@ import pyttsx3
 import pyttsx3
 engine = pyttsx3.init()
 
-engine.say("I will speak this text")
+engine.say("BTS is the biggest band and the best band in the world");
 engine.runAndWait()
