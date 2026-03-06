cfa710029ae1f13b8fccfcc0e62e7a0c00f43e0d
Saloni paney-ux <workspaceworkout0716@gmail.com>
2026-03-06 23:30:14 +0530

    yyp

---
+++ Strings/stringfunctions.py
@@ -0,0 +1,47 @@
+Str= "hi there i am saloni."
+#length of string
+print(len(Str))
+#count of string
+print(Str.count("a"))
+#find the index of string
+print(Str.find("s"))
+#replace the string
+print(Str.replace("saloni","sal"))
+#split the string   
+print(Str.split(" "))
+#capitalize the string
+print(Str.capitalize())
+#lower the string
+print(Str.lower())
+#upper the string
+print(Str.upper())
+#strip the string
+print(Str.strip())
+#startswith the string
+print(Str.startswith("hi"))
+#endswith the string
+print(Str.endswith("saloni."))
+#isalpha the string
+print(Str.isalpha())
+#isdigit the string
+print(Str.isdigit())
+#isalnum the string
+print(Str.isalnum())
+#isupper the string
+print(Str.isupper())
+#islower the string
+print(Str.islower())
+#title the string
+print(Str.title())
+#swapcase the string
+print(Str.swapcase())
+#join the string
+print("-".join(Str))
+#split the string
+print(Str.split(" "))
+#partition the string
+print(Str.partition("am"))
+#zfill the string
+print(Str.zfill(50))
+#center the string
+print(Str.center(50))
