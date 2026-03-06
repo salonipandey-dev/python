fc6127c86fd874b4b3be352f800a3640fd82530b
Saloni paney-ux <workspaceworkout0716@gmail.com>
2026-03-06 21:49:10 +0530

    edit

---
+++ Variablesanddatatypes/operators/assignment.py
@@ -0,0 +1,17 @@
+a=1
+print(a)
+a+=1
+print(a)
+a-=1
+print(a)
+a*=1
+print(a)   
+a/=1
+print(a)
+a%=1 
+print(a) 
+a//=1      
+print(a)
+a**=a
+print(a)  
+#here we used all kind of assignment operators in python while performing them on single variable a .  
