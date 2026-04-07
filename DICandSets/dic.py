09fa8352c68dac134775ab01beb89e2936ecc79e
Saloni paney-ux <workspaceworkout0716@gmail.com>
2026-04-07 17:26:33 +0530

    practice

---
+++ DICandSets/dic.py
@@ -0,0 +1,11 @@
+dict ={0:"hi",1:"hello",2:"bye"}
+print(dict[0])
+print(dict.items())
+print(dict.keys())
+print(dict.values())
+dict[3]="welcome"
+print(dict)
+del dict[1]
+print(dict)
+dict.clear()
+print(dict)
---
+++ DICandSets/practiceset/question1.py
@@ -0,0 +1,9 @@
+dict={
+    "Mein" : "I",
+    "tum":"you",
+    "woh":"he/she/it",
+    "kuch":"some",
+    "thoda aur":"some more"
+}
+print(dict["Mein"])
+print(dict.items()) 
