d568250400a0231673916d97ad38c92689f3e5db
Saloni paney-ux <workspaceworkout0716@gmail.com>
2026-04-19 22:08:59 +0530

    make game

---
+++ project1/main.py
@@ -0,0 +1,24 @@
+'''
+1 for snake
+-1 for water 
+0 for gun
+'''
+computer = -1
+youstr =input("enter your choice :")
+youDict={"snake":1,"water":-1,"gun":0}
+you=youDict[youstr]
+if(computer==-1 and you==1)
+    print("you win")
+elif(computer==1 and you==-1):
+    print("computer wins")
+elif(computer==0 and you==-1):
+    print("you win")
+elif(computer==-1 and you==0):
+    print("computer wins")      
+elif(computer==1 and you==0):
+    print("you win")
+elif(computer==0 and you==1):
+    print("computer wins")
+else:
+    print("draw")
+
